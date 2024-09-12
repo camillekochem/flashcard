e317d3d9dcc226bfa99336a285fa22112c5b26aa
camillekochem <159554720+camillekochem@users.noreply.github.com>
2024-09-12 08:38:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,63 @@
+criaCartao(
+    'Esporte',
+    'Qual país ganhou a copa de 2022',
+    'A Argentina ganhou a copa de 2022'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão',
+    'A capital do Japão é Tokio'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantos lados tem um triângulo',
+    'Um triângulo possui 3 lados'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é goodbye'
+)
+
+criaCartao(
+    'História',
+    'Com quantos anos a rainha Elizabeth II morreu?',
+    'A rainha Elizabeth II morreu com 94 anos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná',
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantas bolas de ouro o Messi tem?',
+    'O jogador de futebol Lionel Messi possui sete bolas de ouro'
+)
+
+criaCartao(
+    'História',
+    'Quem foi a primeira mulher a ganhar o prêmio nobel',
+    'A primeira mulher foi Marie Curie'
+)
+criaCartao(
+    'Esporte',
+    'Quem foi a ginasta medalhista de ouro nas olimpíadas de Paris',
+    'Rebeca Andrade foi a medalhista de ouro'
+)
+
+criaCartao(
+    'História',
+    'Qual país criou o chuveiro elétrico?',
+    'O Brasil'
+)
+criaCartao(
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'A Rússia'
+)
